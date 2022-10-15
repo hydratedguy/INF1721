fd83392a23708aaa0156046d7d1fb8040f914f94
Pierrot <andre.ryc1@gmail.com>
2022-10-15 16:53:36 -0300

    só lembrei de commitar agr

---
+++ T2/graph.py
@@ -0,0 +1,48 @@
+class Graph:
+    def __init__(self):
+        self.nodes = []
+        self.edges = {}
+        self.node_count = 0
+        self.edge_count = 0
+
+    def add_node(self, node):
+        if node not in self.nodes:
+            self.nodes.append(node)
+            self.node_count += 1
+
+    def add_edge(self, node1, node2):
+        if node1 not in self.edges:
+            self.edges[node1] = []
+        if node2 not in self.edges:
+            self.edges[node2] = []
+        if node2 not in self.edges[node1]:
+            self.edges[node1].append(node2)
+            self.edges[node2].append(node1)
+            self.edge_count += 1
+
+    def get_node(self, node):
+        if node in self.nodes:
+            return self.nodes[node]
+        else:
+            return None
+
+    def get_edge(self, node1, node2):
+        if node1 in self.edges:
+            if node2 in self.edges[node1]:
+                return self.edges[node1][node2]
+        return None
+
+    def get_nodes(self):
+        return self.nodes
+
+    def get_edges(self):
+        return self.edges
+
+    def get_node_count(self):
+        return self.node_count
+
+    def get_edge_count(self):
+        return self.edge_count
+
+    def __str__(self):
+        return "Nodes: " + str(self.nodes) + "\nEdges: " + str(self.edges)
