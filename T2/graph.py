cd387a744393281bdb5c9627c365abdf1e87e489
l-luz <lyraluz@yahoo.com.br>
2022-10-24 15:13:18 -0300

    fila visitados build + visualização

--- T2/graph.py
+++ T2/graph.py
@@ -1,35 +1,37 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
 class Graph:
     def __init__(self):
         # self.nodes = {}
         self.edges = {}
         self.node_count = 0
         self.edge_count = 0
+        self.visual = []
 
     def add_node(self, node):
-        if node not in self.edges:
-            self.edges[node] = []
+        if self.edges.get(node, 0) == 0:
             self.node_count += 1
+            self.edges[node] = []
 
     def add_edge(self, node1, node2):
-        if self.edges.get(node1, 0) == 0:
-            self.node_count += 1
-            self.edges[node1] = []
-        
-        if self.edges.get(node2, 0) == 0:
-            self.node_count += 1
-            self.edges[node2] = []
-        
+        self.add_node(node1)
+        self.add_node(node2)
+       
         # Edges list size is max size 4
         if node2 not in self.edges[node1]:
             self.edges[node1].append(node2)
             self.edges[node2].append(node1)
+            self.visual.append([node1,node2])
             self.edge_count += 1
 
+
     def get_node(self, node):
-        if node in self.nodes:
-            return self.nodes[node]
-        else:
-            return None
+        if self.edges.get(node, 0) != 0:
+            return self.edges[node]
+        else :
+            return -1
 
     def get_edge(self, node1, node2):
         if node1 in self.edges:
@@ -38,7 +40,10 @@ class Graph:
         return None
 
     def get_nodes(self):
-        return self.nodes
+        nodes = []
+        for k in self.edges.items():
+            nodes.append(k)
+        return nodes
 
     def get_edges(self):
         return self.edges
@@ -48,6 +53,12 @@ class Graph:
 
     def get_edge_count(self):
         return self.edge_count
+    
+    def visualize(self):
+        G = nx.Graph()
+        G.add_edges_from(self.visual)
+        nx.draw_networkx(G)
+        plt.show()
 
     def __str__(self):
-        return "Nodes: " + str(self.nodes) + "\nEdges: " + str(self.edges)
+        return "Nodes: " + str(self.node_count) + "\nEdges: " + str(self.edge_count)
