784fd2dafb5ca2d5db1598880f62f278c098e7de
l-luz <lyraluz@yahoo.com.br>
2022-10-27 15:13:02 -0300

    commit entrega

--- T2/graph.py
+++ T2/graph.py
@@ -1,14 +1,10 @@
 import networkx as nx
-import matplotlib.pyplot as plt
-
 
 class Graph:
     def __init__(self):
-        # self.nodes = {}
         self.edges = {}
         self.node_count = 0
         self.edge_count = 0
-        self.visual = []
 
     def add_node(self, node):
         if self.edges.get(node, 0) == 0:
@@ -23,9 +19,7 @@ class Graph:
         if node2 not in self.edges[node1]:
             self.edges[node1].append(node2)
             self.edges[node2].append(node1)
-            self.visual.append([node1,node2])
             self.edge_count += 1
-
 
     def get_node(self, node):
         if self.edges.get(node, 0) != 0:
@@ -38,27 +32,6 @@ class Graph:
             if node2 in self.edges[node1]:
                 return self.edges[node1][node2]
         return None
-
-    def get_nodes(self):
-        nodes = []
-        for k in self.edges.items():
-            nodes.append(k)
-        return nodes
-
-    def get_edges(self):
-        return self.edges
-
-    def get_node_count(self):
-        return self.node_count
-
-    def get_edge_count(self):
-        return self.edge_count
-    
-    def visualize(self):
-        G = nx.Graph()
-        G.add_edges_from(self.visual)
-        nx.draw_networkx(G)
-        plt.show()
 
     def __str__(self):
         return "Nodes: " + str(self.node_count) + "\nEdges: " + str(self.edge_count)
